916f3129e8638398fbe245d6d1b0f742c6c35faf
unknown <ameen0saad>
2025-10-01 20:07:54 +0300

    update cookie option

--- Backend/controller/oauthController.js
+++ Backend/controller/oauthController.js
@@ -59,6 +59,7 @@ export const googleAuthCallback = (req, res, next) => {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: process.env.NODE_ENV == 'production',
+      sameSite: 'none',
     };
 
     res.cookie('jwt', token, cookieOption);
